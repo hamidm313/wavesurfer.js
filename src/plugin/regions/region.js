bd16dd79573b2336313365b87b4346df573b9ef2
Hamid313 <hamidmaleki313@gmail.com>
2021-06-13 07:15:30 +0430

    edit region.js

--- src/plugin/regions/region.js
+++ src/plugin/regions/region.js
@@ -257,8 +257,8 @@ export class Region {
         return (start == end ? [start] : [start, end])
             .map((time) =>
                 [
-                    Math.floor((time % 3600) / 60), // minutes
-                    ('00' + Math.floor(time % 60)).slice(-2) // seconds
+                    ('00' + Math.floor((time % 3600) / 60)).slice(-2),  // minutes
+                    ('00' + Math.floor(time % 60)).slice(-2)            // seconds
                 ].join(':')
             )
             .join('-');
